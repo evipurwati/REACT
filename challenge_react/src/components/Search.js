f9a04b6c70cdbd391c50ec4dcaf77f79556ca550
evipurwati <evi@alphatech.id>
2019-03-13 07:30:41 +0700

    update

--- challenge_react/src/components/Search.js
+++ challenge_react/src/components/Search.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // // import Search from 'react-search'
 // import REACTDOM from 'react-dom'
-// import '../css/bootstrap.css'
-// import '../css/bootstrap.min.css'
-// import '../css/style.css';
+import '../css/bootstrap.css'
+import '../css/bootstrap.min.css'
+import '../css/style.css';
 // // import logo from '../logo/logo-alterra-academy.png';
 
 
@@ -51,19 +51,63 @@ import React, { Component } from 'react';
 // import React, { Component } from 'react';
 
 
+// class Search extends Component {
+//   render() {
+//     return (
+//       <div>
+//           <div>
+//             <input onChange={this.props.handleChange} type="text" placeholder="Cari disini" />
+//             <button className="btn btn-sm btn-success px-3">Search</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Search;
+
+// export default Search;
+
+// import React, { Component } from 'react';
+// import '../css/bootstrap.min.css';
+// import '../css/style.css';
+
 class Search extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {value: ''};
+
+  //   this.handlerChange = this.handlerChange.bind(this);
+  // }
+
+  // handlerChange(event) {
+  //   this.setState({ value: event.target.value })
+  // }
+
   render() {
     return (
-      <div>
-          <div>
-            <input onChange={this.props.handleChange} type="text" placeholder="Cari disini" />
-            <button className="btn btn-sm btn-success px-3">Search</button>
+      <div className="top-articles">
+        <div className="col-md-12 row top-head box">
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.props.doSearch} value={this.props.keywords} />
+            {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Q</button> */}
+            {/* <p>{this.state.value}</p> */}
+          </form>
         </div>
+        <br>
+        </br>
       </div>
     );
   }
+
+ 
+  // render() {
+  //   return (
+  //     <section>
+        
+  //     </section>
+  //   )
+  // }
 }
 
-export default Search;
-
-// export default Search;
+export default Search; 
--- challenge_react/src/pages/News.js
+++ challenge_react/src/pages/News.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import '../css/bootstrap.min.css';
 import '../css/listBlog.css';
@@ -12,40 +12,39 @@ import putriayako from '../img/putriayako.jpeg';
 
 const apiKey = "31e59cd2d87747ddbfb14d1c08412411";
 const baseUrl = "https://newsapi.org/v2/"
-const urlHeadline = "https://newsapi.org/v2/top-headlines?country=id&apiKey=31e59cd2d87747ddbfb14d1c08412411"; 
-const urlNews = "https://newsapi.org/v2/everything?q=rampage&apiKey=31e59cd2d87747ddbfb14d1c08412411"; 
+const urlHeadline = "https://newsapi.org/v2/top-headlines?country=id&apiKey=31e59cd2d87747ddbfb14d1c08412411";
+const urlNews = "https://newsapi.org/v2/everything?q=rampage&apiKey=31e59cd2d87747ddbfb14d1c08412411";
 
 class NewsContent extends Component {
-  constructor (props){
+  constructor(props) {
     super(props);
     this.state = {
       listNews: [],
       blog: [],
-      searchText : ""
+      url: ""
     };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.doSearch = this.doSearch.bind(this);
+
   }
 
   componentDidMount = () => {
     const self = this;
-    axios 
+    axios
       .get(urlHeadline)
       .then(function (response) {
         self.setState({ listNews: response.data.articles });
         console.log(response);
       })
-      .catch(function(error) {
-        console.log(error);        
+      .catch(function (error) {
+        console.log(error);
       });
-    axios 
+    axios
       .get(urlNews)
       .then(function (response) {
         self.setState({ blog: response.data.articles });
         console.log(response);
       })
-      .catch(function(error) {
-        console.log(error);        
+      .catch(function (error) {
+        console.log(error);
       });
   }
 
@@ -79,62 +78,90 @@ class NewsContent extends Component {
   //   }
   // };
 
-//   handleChange(e){
-//     this.doSearch(e.target.value);
-//   }
+  handleInputChange = e => {
+    // console.log("event: ", e.target.value)
+    // console.log("Testing Url: ", baseUrl + e.target.value + apiKey)
+    // self.setState({url: baseUrl + e.target.value + apiKey})
+    let value = e.target.value;
+
+    this.setState(
+      {
+        search: value
+      },
+      () => {
+        this.searchNews(value);
+      }
+    );
+  };
+
+  searchNews = async keywords => {
+    console.log("searchNews", keywords);
+    const self = this;
+    if (keywords.length > 2) {
+      try {
+        const response = await axios.get(
+          baseUrl + keywords + apiKey
+        );
+        console.log(response);
+        self.setState({ listNews: response.data.articles })
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
 
 
-//   doSearch(keyword){
-//     let self = this;
-//     let urlSearch = baseUrl + "everything?q="+ keyword + "&apiKey=" + apiKey;
-//     if(keyword.length > 2){
-//         axios.get(urlSearch).then(function(response){
-//             self.setState({listNews : response.data.articles});
-//         }).catch(function(error){
-//             console.log(error);
-//         });
-//     }
-    
-// }
-  
+  //   doSearch(keyword){
+  //     let self = this;
+  //     let urlSearch = baseUrl + "everything?q="+ keyword + "&apiKey=" + apiKey;
+  //     if(keyword.length > 2){
+  //         axios.get(urlSearch).then(function(response){
+  //             self.setState({listNews : response.data.articles});
+  //         }).catch(function(error){
+  //             console.log(error);
+  //         });
+  //     }
+
+  // }
+
   render() {
     console.log("here")
-    const {listNews} = this.state;
-    const {blog} = this.state;
-    const is_login = JSON.parse(localStorage.getItem("is_login"));
+    const { listNews } = this.state;
+    const { blog } = this.state;
+    // const is_login = JSON.parse(localStorage.getItem("is_login"));
     return (
       <div className="App">
-            <meta charset="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
         <div class="row">
-            <div class="col-md-12">
-                <Navbar />
-            </div>
+          <div class="col-md-12">
+            <Navbar />
+          </div>
         </div>
-        
+
         <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <Search title="Cari" placeholder="type keyword.."/><br></br>
-                    {listNews.slice(0,5).map((item, key) => {
-                      const title = item.title !== null ? item.title : "";
-                      return <ListNews key={key} title={title} index={key}/>;
-                    })}
-                </div>
-                <div class="col-md-8">
-                    {blog.slice(0,5).map((item, key) => {
-                      const src_img = item.urlToImage === null ? az : item.urlToImage;
-                      const content = item.content !== null ? item.content: "";
-                      const title = item.title !== null ? item.title : "";
-                      const index = key;
-                      return <Blog key={key} title={title} img={src_img} content={content} index={key}/>;
-                    })}
-                </div>
+          <div class="row">
+            <div class="col-md-4">
+              <Search doSearch={(e) => this.handleInputChange(e)} /><br></br>
+              {listNews.slice(0, 5).map((item, key) => {
+                const title = item.title !== null ? item.title : "";
+                return <ListNews key={key} title={title} index={key} />;
+              })}
             </div>
+            <div class="col-md-8">
+              {blog.slice(0, 5).map((item, key) => {
+                const src_img = item.urlToImage === null ? az : item.urlToImage;
+                const content = item.content !== null ? item.content : "";
+                const title = item.title !== null ? item.title : "";
+                const index = key;
+                return <Blog key={key} title={title} img={src_img} content={content} index={key} />;
+              })}
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -172,7 +199,7 @@ class NewsContent extends Component {
 //               <Navbar />
 //           </div>
 //       </div>
-      
+
 //       <div class="container">
 //           <div class="row">
 //               <div class="col-md-4">
